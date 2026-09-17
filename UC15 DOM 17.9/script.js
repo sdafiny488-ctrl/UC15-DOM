8813e3c1481c7f2e708b8660494b8ab2291631a4
sdafiny488-ctrl <sdafiny488@gmail.com>
2026-09-17 10:38:07 -0300

    Add files via upload

---
+++ UC15 DOM 17.9/script.js
@@ -0,0 +1,9 @@
+function mudartexto() {
+    document.getElementById
+    ("titulo").textContent = "Você clicou no botão!";
+}
+
+function mudarcor() {
+    document.getElementById("mensagem").style.color =
+    "blue";
+}
